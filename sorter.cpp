392d76035a34e1e5509e579dac57f9ec53502217
Eddeyosaurus <ec132297@l-rss129-73662.math.cs.edinboro.edu>
2017-03-18 14:59:57 -0400

    debug

--- sorter.cpp
+++ sorter.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <fcntl.h>
 #include <algorithm>
+#include <vector>
+
 using namespace std;
 
 int main(int argc, char* argv[])
@@ -16,10 +18,11 @@ int main(int argc, char* argv[])
         perror("Usage: ./sorter [inputFile] [outputFile]\nExit Status: ");
         exit(EXIT_FAILURE);
     }
-    
+
     int a = -1, b = -1, a1 = -1, b1 = -1;
-    int result[100];
+    int initArr[100];
     int left_a[25], right_a[25], left_b[25], right_b[25];
+    vector<int> bottom, top, result;
     int readFD = 0, writeFD = 1, openFlags;
     mode_t mode;
     int* buffer;
@@ -27,9 +30,9 @@ int main(int argc, char* argv[])
 
 
     openFlags = O_RDONLY;
-    
-    mode = S_IRUSR | S_IWUSR | 
-           S_IRGRP | S_IWGRP | 
+
+    mode = S_IRUSR | S_IWUSR |
+           S_IRGRP | S_IWGRP |
            S_IROTH | S_IWOTH;
 
 
@@ -38,47 +41,48 @@ int main(int argc, char* argv[])
 
     for(int i = 0; i < 100; i++)
     {
-       read(readFD, &result[i], sizeof(int));
+       read(readFD, &initArr[i], sizeof(int));
        //cout << buffer << endl;
     }
 
-
     int count = 0;
     for(int i = 0; i < 100; i++)
-    {   
+    {
         if(count == 25)
             count = 0;
 
         if(i < 25)
-            left_a[count] = result[i];
+            left_a[count] = initArr[i];
         else if(i < 50)
-            right_a[count] = result[i];
+            right_a[count] = initArr[i];
         else if(i < 75)
-            left_b[count] = result[i];
+            left_b[count] = initArr[i];
         else if(i < 100)
-            right_b[count] = result[i];
+            right_b[count] = initArr[i];
 
         count++;
     }
-    
+
+
     /*for(int i = 0; i < 100; i++){
         if(count == 25)
             count = 0;
 
-        
+
         if(i < 25)
-            cout << left_a[count] << endl; 
+            cout << left_a[count] << endl;
         else if(i < 50)
             cout << right_a[count] << endl;
         else if(i < 75)
             cout << left_b[count] << endl;
         else if(i < 100)
             cout << right_b[count] << endl;
-           
+
         count++;
     }*/
 
     a = fork();
+
 
     if(a == 0){
         b = fork();
@@ -87,25 +91,68 @@ int main(int argc, char* argv[])
             b1 = fork();
 
             if(b1 == 0){
-                cout << "\n\n\n\nthis is b1\n\n\n\n";
+                //cout << "\n\n\n\nthis is b1\n\n\n\n";
+                //sleep(10);
+                //cout << "\n\nIn b1\n\n";
+                sort(left_b, left_b+25);
+                cout << endl << endl;
+                /*for(int i = 0; i < 25; i++){
+                    cout << left_b[i] << endl;
+                }*/
             }
-            else
-                cout << "\n\nt\n\nhis is b\n\n\n\n";
+            else{
+              //cout << "\n\n\n\nthis is b\n\n\n\n";
+              waitpid(b1, NULL, 0);
+              /*for(int i = 0; i < 25; i++){
+                  cout << left_b[i] << endl;
+              }*/
+              sort(right_b, right_b+25);
+              cout << endl << endl;
+              /*for(int i = 0; i < 25; i++){
+                  cout << top[i] << endl;
+              }*/
+              //cout << "\n\nIn b\n\n";
+            }
         }
         else {
             a1 = fork();
 
             if(a1 == 0){
-                cout << "\n\n\n\nthis is a1\n\n\n\n";
+                //cout << "\n\n\n\nthis is a1\n\n\n\n";
+                //sleep(9);
+                //cout << "\n\nIn a1\n\n";
+                sort(right_a, right_a+25);
+                cout << endl << endl;
+                /*for(int i = 0; i < 25; i++){
+                    cout << right_a[i] << endl;
+                }*/
             }
-            else
-                cout << "\n\n\n\nthis is a\n\n\n\n";
+            else{
+                //cout << "\n\n\n\nthis is a\n\n\n\n";
+                waitpid(a1, NULL, 0 );
+                waitpid(b, NULL, 0);
+                sort(left_a, left_a+25);
+                cout << endl << endl;
+                /*for(int i = 0; i < 25; i++){
+                    cout << left_a[i] << endl;
+                }*/
+                //cout << "\n\nIn a\n\n";
+              }
         }
-           
+        merge(left_a, left_a+25, right_a, right_a+25, bottom.begin());
+        for(int i = 0; i < bottom.size(); i++){
+            cout << bottom[i] << endl;
+        }
+        waitpid(b, NULL, 0);
+        merge(left_b, left_b+25,right_b, right_b+25, top.begin());
     }
-       
+    //waitpid(a, NULL, 0);
+    //merge(bottom.begin(), bottom.end(), top.begin(), top.end(), result.begin());
 
-   
+    /*for(int i = 0; i < 100; i++){
+        cout << result[i] << endl;
+    }*/
+
 
     /*for(int i = 0; i < 100; i++){
         if(count == 25){
@@ -113,16 +160,16 @@ int main(int argc, char* argv[])
             cout << endl << endl;
         }
 
-        
+
         if(i < 25)
-            cout << left_a[count] << endl; 
+            cout << left_a[count] << endl;
         else if(i < 50)
             cout << right_a[count] << endl;
         else if(i < 75)
             cout << left_b[count] << endl;
         else if(i < 100)
             cout << right_b[count] << endl;
-           
+
         count++;
     }*/
 
